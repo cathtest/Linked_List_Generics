8b6bd86ba7e55ddd9a50406dba2d6b94282225b1
Katsiaryna Hrynchuk <Katsiaryna_Hrynchuk@epam.com>
2018-06-12 19:51:02 +0300

    changes

---
+++ src/FlowerType.java
@@ -0,0 +1,16 @@
+public class FlowerType {
+
+
+    String s;
+    public FlowerType(String s) {
+        this.s= this.s;
+    }
+
+
+
+
+    public String getS() {
+        return s;
+    }
+
+}
---
+++ src/Linked.java
@@ -0,0 +1,215 @@
+public class Linked {
+
+    Flower firstFlower;
+    Flower lastFlower;
+
+    public void first(FlowerType flowerType) {
+
+        Flower theNewFlower = new Flower(flowerType);
+
+
+
+        if (isEmpty()) {
+
+            lastFlower = theNewFlower;
+
+        } else {
+
+            firstFlower.previous = theNewFlower;
+
+        }
+
+        theNewFlower.next = firstFlower;
+
+        firstFlower = theNewFlower;
+
+    }
+
+    public void last(FlowerType flowerType) {
+
+        Flower theNewFlower = new Flower(flowerType);
+
+
+
+        if (isEmpty()) {
+
+            firstFlower = theNewFlower;
+
+        } else {
+
+
+            lastFlower.next = theNewFlower;
+
+            theNewFlower.previous = lastFlower;
+
+        }
+
+        lastFlower = theNewFlower;
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        Linked theLinkedList = new Linked();
+
+
+        theLinkedList.first(new FlowerType("Roses are red"));
+        theLinkedList.first(new FlowerType("Violets are blue"));
+        theLinkedList.first(new FlowerType("Daffodils are unknown"));
+        theLinkedList.last(new FlowerType("Daisies are rosy"));
+        theLinkedList.last(new FlowerType("Daisies are rosy"));
+
+        theLinkedList.display();
+
+        System.out.println("\n");
+
+        FlowersIt flowers = new FlowersIt(theLinkedList);
+
+        flowers.currentFlower.display();
+
+        System.out.println(flowers.hasNext());
+
+        flowers.next();
+
+        flowers.currentFlower.display();
+
+        flowers.remove();
+
+        flowers.currentFlower.display();
+
+
+    }
+
+
+    public boolean isEmpty() {
+
+        return (firstFlower == null);
+
+    }
+
+
+
+    public void display() {
+
+        Flower theLink = firstFlower;
+
+        while (theLink != null) {
+
+            theLink.display();
+
+            System.out.println("Next Flower: " + theLink.next);
+
+            theLink = theLink.next;
+
+            System.out.println();
+
+        }
+
+    }
+
+}
+
+class Flower {
+
+    public FlowerType flowerType;
+
+
+    public Flower next;
+
+    public Flower previous;
+
+    public Flower(FlowerType flowerType) {
+
+        this.flowerType = flowerType;
+
+
+    }
+
+    public void display() {
+
+        System.out.println(flowerType + ": "  + " Happy Flowers Store");
+
+    }
+
+
+}
+
+
+
+class FlowersIt {
+
+    Flower currentFlower;
+    Flower previousFlower;
+
+    Linked theFlowers;
+
+
+
+    FlowersIt(Linked theFlowers) {
+
+        this.theFlowers = theFlowers;
+
+        currentFlower = theFlowers.firstFlower;
+        previousFlower = theFlowers.lastFlower;
+
+    }
+
+    public boolean hasNext() {
+
+        if (currentFlower.next != null) {
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    public Flower next() {
+
+        if (hasNext()) {
+
+            previousFlower = currentFlower;
+            currentFlower = currentFlower.next;
+
+            return currentFlower;
+
+        }
+
+        return null;
+
+    }
+
+    public void remove() {
+
+
+
+        if (previousFlower == null) {
+
+            theFlowers.firstFlower = currentFlower.next;
+
+        } else {
+            previousFlower.next = currentFlower.next;
+
+
+
+            if (currentFlower.next == null) {
+
+
+                currentFlower = theFlowers.firstFlower;
+                previousFlower = null;
+
+            } else {
+
+                currentFlower = currentFlower.next;
+
+            }
+
+        }
+
+    }
+
+}
